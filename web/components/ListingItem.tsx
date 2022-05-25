e82969b1dc8615cc36e4cd66cb51763bd0b29ffa
TheLDB <ldb@erikboles.com>
2022-05-25 04:10:21 -0600

    Added dynamic listings list and map[ped iit to a UI element

---
+++ web/components/ListingItem.tsx
@@ -0,0 +1,9 @@
+const ListingItem = () => {
+    return (
+        <div className="w-full h-full bg-green-500">
+
+        </div>
+    )
+}
+
+export default ListingItem;
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,12 +1,39 @@
 import type { NextPage } from 'next'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import MenuBar from '../components/MenuBar'
 
 const Home: NextPage = () => {
+  const [ listings, setListings ] = useState([
+    {
+      id: 0
+    }
+  ]);
+
+  useEffect(() => {
+    const addListing = [...listings, {
+      id: 1
+    }];
+    setListings(addListing);
+  }, [])
+
   return (
     <div className="w-screen h-screen bg-site-neutral">
       <div className="w-full h-[15%]">
         <MenuBar />
+      </div>
+      <div className="w-full h-[85%]">
+        <div className="w-full h-1/6 SPACING-DONT-TOUCH" />
+        <div className="w-full h-1/6 flex justify-center items-center">
+          <h1 className="text-black text-7xl font-indie">Live Listings</h1>
+        </div>
+        <div className="w-full h-4/6 bg-black">
+          {listings.map((listi) => {
+            return (
+              <h1 className="text-white">{listi.id}</h1>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
