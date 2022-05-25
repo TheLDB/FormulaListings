a87bb3fd5c93da1b642def3fe85527cda52ccee1
TheLDB <ldb@erikboles.com>
2022-05-25 04:54:11 -0600

    Added about and settings buttons

--- web/components/MenuBar.tsx
+++ web/components/MenuBar.tsx
@@ -11,15 +11,23 @@ const MenuBar = () => {
 
     return (
         <div className="w-full h-full flex flex-row">
-            <div className="w-1/3 h-full">
-
+            <div className="w-1/3 h-full flex items-center pl-4">
+                <button className="btn w-1/3 h-2/3 flex justify-center items-center">
+                    <h1 className="text-white font-indie text-3xl">
+                        About
+                    </h1>
+                </button>
             </div>
             <div className="w-1/3 h-full flex flex-col justify-center items-center -space-y-4">
                 <h1 className="text-black text-7xl font-indie">Formula Listings</h1>
                 <h1 className={`${liveRed ? 'text-red-500' : 'text-black'} text-6xl font-indie transition-all ease-in-out duration-700`}>Live</h1>
             </div>
-            <div className="w-1/3 h-full">
-                
+            <div className="w-1/3 h-full flex items-center justify-end pr-4">
+                <button className="btn w-1/3 h-2/3">
+                    <h1 className="text-white font-indie text-3xl">
+                        Settings
+                    </h1>
+                </button>
             </div>
         </div>
     )
